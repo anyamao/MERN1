3f7944f5e38c3619b5d4d96239b79b7a2f3ae466
vika <vikashinder@gmail.com>
2026-02-05 14:57:37 +0700

    Looks like the basic generation with drizzle is working,yay

--- schema.ts
+++ schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, text } from "drizzle-orm/pg-core";
 export const test = pgTable("test", {
   id: serial("id").primaryKey(),
+  name: varchar("name"),
 });
